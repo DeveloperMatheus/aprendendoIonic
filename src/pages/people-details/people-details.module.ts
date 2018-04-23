e61a517be2bf23480be71c02c9e72c81719e53ac
Matheus  de Sousa Menezes <matheus.developer@gmail.com>
2018-04-23 04:05:45 -0300

    Adição do arquivo .json da animação da curtida, e botão de play

--- src/pages/people-details/people-details.module.ts
+++ src/pages/people-details/people-details.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { PeopleDetailsPage } from './people-details';
+import { LottieAnimationViewModule } from 'ng-lottie';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { PeopleDetailsPage } from './people-details';
   ],
   imports: [
     IonicPageModule.forChild(PeopleDetailsPage),
+    LottieAnimationViewModule.forRoot()
   ],
 })
 export class PeopleDetailsPageModule {}
--- src/pages/people-details/people-details.ts
+++ src/pages/people-details/people-details.ts
@@ -14,13 +14,60 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'people-details.html',
 })
 export class PeopleDetailsPage {
+  isHearted: any;
+  anim: any;
   pessoa:any;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  lottieConfig: any;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    ) {
     this.pessoa = this.navParams.get('pessoa');
+    
+    this.lottieConfig = {
+      path: 'assets/curtida.json',
+      autoplay: false,
+      loop: false,
+      
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PeopleDetailsPage');
   }
+  
+  handleAnimation(anim: any) {
+    this.anim = anim;
+  }
+
+//   handleAnimation(anim: any) {
+//     this.anim = anim;
+
+//     if (this.isHearted) {
+//         this.anim.goToAndStop(25, true); //last frame. on state.
+//         this.isHearted = false;
+//     }
+//     else {
+//         this.anim.goToAndStop(0, true); //first frame. off state.
+//         this.isHearted = true;
+//     }
+// }
+
+  // stop() {
+  //   this.anim.stop();
+  // }
+
+  play() {
+    this.anim.play();
+  }
+
+  // pause() {
+  //   this.anim.pause();
+  // }
+
+  // setSpeed(speed: number) {
+  //   this.animationSpeed = speed;
+  //   this.anim.setSpeed(speed);
+  // }
 
 }
